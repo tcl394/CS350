cd6479c45cf7abb7efadda627a16504d622477fc
Casey Burklow <burk0683@bears.unco.edu>
2017-10-31 04:50:07 -0600

    Started C# Conversion

---
+++ Exercises/Results/guzm3915/Development_Exercise/readCSV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace crud
+{
+    class readCSV
+    {
+        public static Dictionary<string, string> importData()
+        {
+            Dictionary<string, string> temp = new Dictionary<string, string>();
+            foreach (string line in File.ReadLines("authors.txt"))
+            {
+                temp.Add(line.Split(",")[0], line.Split(",")[1]);
+            }
+            return temp;
+        }
+    }
+}
---
+++ Exercises/Results/guzm3915/Development_Exercise/writeCSV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace crud
+{
+    class writeCSV
+    {
+
+        public static bool write(Dictionary<string, string> authors)
+        {
+
+            string[] temp = new string[authors.Count];
+
+            foreach (string name in authors.Keys)
+            {
+                
+            }
+
+            return false;
+        }
+
+    }
+}
