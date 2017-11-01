aeeb77bcaf6e949f56388d33be5ac2ab9558cfae
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-01 15:18:59 -0600

    Update social_net_crud.py

--- Exercises/Results/jone2032/social_net_crud.py
+++ Exercises/Results/jone2032/social_net_crud.py
@@ -1,7 +1,10 @@
 from csv import reader, writer
+import os.path
+
 
 # Read rows and columns from a CSV File
 def read_csv(filepath):
+    create_csv(filepath)
     with open(filepath) as f:
         return [row for row in reader(f) if row]
 
@@ -13,19 +16,29 @@ def write_csv(filepath, data):
         for row in data:
             w.writerow(row)
 
+# create csv file
+def create_csv(filename):
+    if '.csv' not in filename:
+        filename += '.csv'
+    if os.path.isfile(filename)== False:
+        open(filename, 'w')
+    
+        
 # test functionality with author csv file
 def test_author():
     path = 'author.csv'
+    create_csv(path)
     data = [['test','test'],['test','test']]
     write_csv(path, data)
-    text = read_csv(path)
+    text = read_author()
     assert(text==data)
-    assert(text!=data)
+    #assert(text!=data)
 
 
 # test functionality with articles csv file
 def test_article():
     path = 'articles.csv'
+    create_csv(path)
     data = [['test','test'],['test2','test2']]
     write_csv(path, data)
     text = read_csv(path)
@@ -51,6 +64,7 @@ def other():
 
 
 def create_author(name, email):
+    create_csv('author.csv')
     path = 'author.csv'
     text = read_csv(path)
     data = [[name, email]]
@@ -61,9 +75,14 @@ def create_author(name, email):
     
 
 def read_author():
-    ## stuff
-    pass
+    path = 'author.csv'
+    create_csv(path)
+    return read_csv(path)
 
+def test_read_author():
+    text = read_author()
+    print (text)
+    
 def update_author():
     ##stuff
     pass
@@ -72,6 +91,13 @@ def delete_author():
     ##stuff
     pass
 
+def test_create_csv():
+    create_csv('article')
+    assert(os.path.isfile('article.csv'))
+
 if __name__ == '__main__':
+    test_create_csv()
+    test_author()
+
     #other()
     create_author('nik','jone2032@bears.unco.edu')
