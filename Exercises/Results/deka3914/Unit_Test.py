b37cfe4e57e5c1cb9a8667c4ff81f8e564f61ab5
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-08 14:53:27 -0700

    Update Unit_Test.py

--- Exercises/Results/deka3914/Unit_Test.py
+++ Exercises/Results/deka3914/Unit_Test.py
@@ -11,3 +11,26 @@ def test_create():
   assert(x != none)
   return x
 
+def set_info(x,first,last,email):
+    x.first_name = first
+    x.last_name = last
+    x.email = email
+    return x
+
+
+def set_users():
+    a = []
+    one = user()
+    one.firstName = 'Bob'
+    one.lastName = 'Smith'
+    one.email = 'BS@Here.com'
+ 
+    two = user()
+    two.firstName = 'Mary'
+    two.lastName = 'Jane'
+    two.email = 'MJ@Aye.com
+    
+    a.append(one)
+    a.append(two)
+
+    return a
