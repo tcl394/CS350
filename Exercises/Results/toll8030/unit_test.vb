a4e27db2d7b3da8613c4e5b0dc30e23b18a1e160
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-12 18:55:52 -0700

    Add files via upload

---
+++ Exercises/Results/toll8030/unit_test.vb
@@ -0,0 +1,60 @@
+﻿Module unit_test
+    Function test_user() As Boolean
+        Dim t As Boolean = True
+
+        t = test_create()
+
+        If t Then
+            MsgBox("ok")
+        Else
+            MsgBox("not ok")
+        End If
+        Return t
+    End Function
+
+    Function test_create() As Boolean
+        Dim t As Boolean = False
+        Dim User As Object = User().ToString
+        If User IsNot Nothing Then
+            t = True
+        End If
+        Return t
+    End Function
+
+    Sub User()
+        Dim first, last, email As String
+        first = ""
+        last = ""
+        email = ""
+        Dim name As Array
+        name = {first, last}
+
+
+    End Sub
+
+    Sub UserList(user)
+        Dim list As ArrayList
+        list.Add(user)
+
+    End Sub
+
+    Function test_name() As Boolean
+        Dim t As Boolean = False
+        If User().name IsNot Nothing Then
+            t = True
+        End If
+        Return t
+    End Function
+    Function test_email() As Boolean
+        Dim t As Boolean = False
+        If User().email IsNot Nothing Then
+            t = True
+        End If
+        Return t
+    End Function
+
+    Sub Main()
+        test_user()
+    End Sub
+
+End Module
