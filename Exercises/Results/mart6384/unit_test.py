fd6c013c41ddc2a888021fd9e54082f5863bec31
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-19 13:12:45 -0700

    Update unit_test.py

--- Exercises/Results/mart6384/unit_test.py
+++ Exercises/Results/mart6384/unit_test.py
@@ -1,3 +1,46 @@
-def test_user();
-def test_create();
+from user import user
+
+def test_user():  
+    userOne = test_create()
+    test_first(userOne, "Jane")
+    test_last(userOne, "Doe")
+    test_email(userOne, "JaneDoe@gmail.com")
+try:
+    test_user()
+    print("It worked")
+except:
+    print("did not work")
+    
+def test_create():
+  try:
+        userOne = User("","","")
+        return userOne
+    except:
+        print ('this does not exist')
+        
+ def test_first(userOne, firstName):
+    try:
+        userOne.first = firstName
+    except:
+        print ("first name cannot be added ")
+        
+def test_last(userOne, lastName):
+    try:
+        userOne.last = lastName
+    except:
+        print ("last name cannot be added")
+        
+def test_email(userOne, email):
+    try:
+        userOne.email = email
+    except:
+        print (“Email cannot be added")
+ 
+def test_create():
+    try:
+        userOne = User("","","")
+        return userOne
+    except:
+        print ('This does not exist')
+    
 def User;
