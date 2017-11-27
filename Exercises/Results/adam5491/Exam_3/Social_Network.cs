26fffb219b57ae6943d9eb10a5fcca45a60dd583
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-27 14:51:21 -0700

    Add files via upload

---
+++ Exercises/Results/adam5491/Exam_3/Social_Network.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_350_Exam_3
+{
+    class Social_Network
+    {
+        public class Person
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public int UserID { get; set; }
+            public List<int> Friends { get; set; }
+            public List<string> Posts { get; set; }
+            public static bool Create_User(String UserName, String Password, int ID)
+            {
+                try
+                {
+                    Person person = new Person();
+                    person.UserName = UserName;
+                    person.Password = Password;
+                    person.UserID = ID;
+                    string[] personInfo = { person.UserID.ToString(), person.UserName, person.Password };
+                    System.IO.File.AppendAllText(@"C:\Users\Tyler\source\repos\CS 350 Exam 3\social_network_app\bin\user_list.csv", personInfo[0] + "," + personInfo[1] + "," + personInfo[2] + "\n");
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            public static Person Lookup_Person(int UserID)
+            {
+                Person person = new Person();
+                var Lines = File.ReadLines("C:/Users/Tyler/source/repos/CS 350 Exam 3/social_network_app/bin/user_list.csv");
+                foreach (var line in Lines)
+                {
+                    if (line.Split(',')[0].Equals(UserID))
+                    {
+                        person.UserID = Convert.ToInt32(line.Split(',')[0]);
+                        person.UserName = line.Split(',')[1];
+                        person.Password = line.Split(',')[2];
+                        person.Friends = new List<int> { };
+                        person.Friends.Add(Convert.ToInt32(line.Split(',')[3]));
+                        return person;
+                    }
+                }
+                return person;
+            }
+        }
+        public class Friend
+        {
+            public static bool Add_Friend(int FirstID, int SecondID)
+            {
+                try
+                {
+                    Person PersonOne = Person.Lookup_Person(FirstID);
+                    Person PersonTwo = Person.Lookup_Person(SecondID);
+                    PersonOne.Friends = new List<int> { };
+                    PersonTwo.Friends = new List<int> { };
+                    PersonOne.Friends.Add(SecondID);
+                    PersonTwo.Friends.Add(FirstID);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            public static List<int> List_Friends(int LookupID)
+            {
+                Person person = Person.Lookup_Person(LookupID);
+                return person.Friends;
+            }
+            public static bool Remove_Friend(int FirstID, int SecondID)
+            {
+                try
+                {
+                    Person PersonOne = Person.Lookup_Person(FirstID);
+                    Person PersonTwo = Person.Lookup_Person(SecondID);
+                    PersonOne.Friends = new List<int> { };
+                    PersonTwo.Friends = new List<int> { };
+                    PersonOne.Friends.Remove(SecondID);
+                    PersonTwo.Friends.Remove(FirstID);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public class Post
+        {
+            public static List<string> List_Topics(int UserID)
+            {
+                Person person = new Person();
+                person = Person.Lookup_Person(UserID);
+                return person.Posts;
+            }
+            public static bool AddTopic(int UserID, string Content)
+            {
+                try
+                {
+                    Person person = new Person();
+                    person = Person.Lookup_Person(UserID);
+                    person.Posts = new List<string> { };
+                    person.Posts.Add(Content);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            public static bool RemoveTopic(int UserID, string Content)
+            {
+                try
+                {
+                    Person person = new Person();
+                    person = Person.Lookup_Person(UserID);
+                    person.Posts = new List<string> { };
+                    person.Posts.Remove(Content);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public class MasterSystem
+        {
+            public static void MasterReset()
+            {
+                System.IO.File.WriteAllText(@"C:\Users\Tyler\source\repos\CS 350 Exam 3\social_network_app\bin\user_list.csv", string.Empty);
+            }
+            public static bool ExportData(String filepath)
+            {
+                try
+                {
+                    File.Copy(@"C:\Users\Tyler\source\repos\CS 350 Exam 3\social_network_app\bin\user_list.csv", filepath, true);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            public static bool ImportData(String filepath)
+            {
+                try
+                {
+                    File.Copy(filepath, @"C:\Users\Tyler\source\repos\CS 350 Exam 3\social_network_app\bin\user_list.csv", true);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Exercises/Results/adam5491/Exam_3/Social_Network_Test.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_350_Exam_3
+{
+    class Social_Network_Test
+    {
+        static void Main(string[] args)
+        {
+            Test_User_Create();
+            Test_User_Lookup();
+            Test_Add_Friend();
+            Test_List_Friends();
+            Test_Remove_Friends();
+            Test_AddTopic();
+            Test_List_Topics();
+            Test_RemoveTopic();
+            Test_ExportData();
+            Test_ImportData();
+            Test_Master_Reset_UserInfo();
+
+        }
+        public static void Test_User_Create()
+        {
+            bool success = Social_Network.Person.Create_User("TAdams", "iLoveCats", 1004);
+            Debug.Assert(success == true, "User not properly created.");   
+        }
+        public static void Test_User_Lookup()
+        {
+            Social_Network.Person person = Social_Network.Person.Lookup_Person(1002);
+            Debug.Assert(person != null, "User does not exist.");
+        }
+        public static void Test_Add_Friend()
+        {
+            bool success = Social_Network.Friend.Add_Friend(1002, 1003);
+            Debug.Assert(success == true, "Failed to add friend.");
+        }
+        public static void Test_List_Friends()
+        {
+            Social_Network.Friend.List_Friends(1000);
+        }
+        public static void Test_Remove_Friends()
+        {
+            bool success = Social_Network.Friend.Remove_Friend(1002, 1003);
+            Debug.Assert(success == true, "Failed to remove friend.");
+        }
+        public static void Test_AddTopic()
+        {
+            bool success = Social_Network.Post.AddTopic(1003, "Cats");
+            Debug.Assert(success == true, "Failed to add topic.");
+        }
+        public static void Test_List_Topics()
+        {
+            Social_Network.Post.List_Topics(1003);
+        }
+        public static void Test_RemoveTopic()
+        {
+            bool success = Social_Network.Post.RemoveTopic(1003, "Cats");
+            Debug.Assert(success == true, "Failed to remove topic.");
+        }
+        public static void Test_Master_Reset_UserInfo()
+        {
+            Social_Network.MasterSystem.MasterReset();
+            var csvFileLength = new System.IO.FileInfo("C:/Users/Tyler/source/repos/CS 350 Exam 3/social_network_app/bin/user_list.csv").Length;
+            Debug.Assert(csvFileLength == 0, "User list not successfully reset.");
+        }
+        public static void Test_ExportData()
+        {
+            bool success = Social_Network.MasterSystem.ExportData("C:/Users/Tyler/Desktop/test.csv");
+            Debug.Assert(success == true, "Failed to export data. Check file path.");
+        }
+        public static void Test_ImportData()
+        {
+            bool success = Social_Network.MasterSystem.ImportData("C:/Users/Tyler/Desktop/test.csv");
+            Debug.Assert(success == true, "Failed to import data. Check file path.");
+        }
+    }
+}
