29306b74f2ba5007643e36fa91f9c09d92d58f11
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-27 14:34:48 -0700

    Add files via upload

---
+++ Exercises/Results/dixo7800/Exam 3/Social_Network.cs
@@ -0,0 +1,446 @@
+﻿using CS_350_Exam_3;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CS_350_Exam_3_Final_Version
+{
+    class Social_Network
+    {
+        static void Main(string[] args)
+        {
+            //This must be run everytime.
+            Social_Networking_System sys = new Social_Networking_System();
+            sys.ResetData();
+
+
+            Social_Network_Test.AllTests();
+        }
+    }
+
+    class Friend
+    {
+        //maybe the person should have a list of friends of type friend
+        List<string> Friends = new List<string>();
+
+        public void ListFriends()
+        {
+            Friends = Tools.ReadFileSplitNewLine(Social_Networking_System.friendsFilePath);
+
+            /*foreach (string f in friendsList)
+            {
+                //Console.WriteLine(f);
+                Friends.Add(f);
+            }*/
+
+            foreach(string f in Friends)
+            {
+                Console.WriteLine(f);
+            }
+
+        }
+
+        public void AddFriendship(Person one, Person two)
+        {
+            //Console.WriteLine("Username: " + one.Username);
+            Friends.Add(one.Username + "," + two.Username);
+
+            string line = one.Username + "," + two.Username;
+
+            Console.WriteLine("Friendship " + line + " added");
+
+            /*foreach(string s in Friends)
+            {
+                Console.WriteLine(s);
+            }*/
+
+            Tools.WriteFile(Social_Networking_System.friendsFilePath, line);
+
+
+        }
+
+        public void RemoveFriendship(Person one, Person two)
+        {
+            Friends = Tools.ReadFileSplitNewLine(Social_Networking_System.friendsFilePath);
+
+            Friends.Remove(one.Username + "," + two.Username + "\r");
+            Friends.Remove(two.Username + "," + one.Username + "\r");
+
+            string allRemainingFriends = "";
+
+            foreach(string f in Friends)
+            {
+                allRemainingFriends = allRemainingFriends + f + "\n";
+            }
+
+            Tools.WriteNotAppendingFile(Social_Networking_System.friendsFilePath, allRemainingFriends);
+
+            Console.WriteLine("Freindship " + one.Username + "," + two.Username + " removed");
+
+        }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+        public void Register(string fname, string lname, string email, string uname, string pw)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Email = email;
+            Username = uname;
+            Password = pw;
+
+            AddUser();
+
+        }
+
+        public void Login(string uname, string pw)
+        {
+            Username = uname;
+            Password = pw;
+
+            //Console.WriteLine(Username);
+            //Console.WriteLine(Password);
+
+            List<string> usersfile = Tools.ReadFileSplitComma(Social_Networking_System.PersonFilePath);
+
+            List<int> needsRemoval = new List<int>();
+
+            foreach(string u in usersfile)
+            {
+                if(u == "" || u == null)
+                {
+                    needsRemoval.Add(usersfile.IndexOf(u));
+                }
+            }
+
+            foreach(int n in needsRemoval)
+            {
+                usersfile.RemoveAt(n);
+            }
+
+            /*foreach(string r in usersfile)
+            {
+                Console.WriteLine(r);
+            }*/
+
+            bool userFound = false;
+            bool passFound = false;
+
+            for (int i = 3; i < usersfile.Count; i = i + 5)
+            {
+                string test = usersfile[i];
+                //Console.WriteLine(test + " " + Username);
+                if (test == Username)
+                {
+                    //Console.WriteLine("User exists");
+                    userFound = true;
+                    break;
+                }
+            }
+
+            for (int i = 4; i < usersfile.Count; i = i + 5)
+            {
+                string test = usersfile[i];
+                //Console.WriteLine(test + " " + Password);
+                if (test == Password)
+                {
+                    //Console.WriteLine("Password found");
+                    passFound = true;
+                    break;
+                }
+            }
+
+            if (userFound == true && passFound == true)
+            {
+                Console.WriteLine("Successful Login");
+            }
+            else
+            {
+                Console.WriteLine("Login failed");
+            }
+
+        }
+
+        private void AddUser()
+        {
+
+            string line = FirstName + "," + LastName + "," + Email + "," + Username + "," + Password;
+
+            Tools.WriteFile(Social_Networking_System.PersonFilePath, line);
+
+            Console.WriteLine("Successful Registration!");
+        }
+    }
+
+    class Post
+    {
+        public string Topic { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+
+
+        public void postWarning()
+        {
+            Console.WriteLine("\nPlease do not have any commas in your topic, your title, or your body. \nOtherwise errors will occur. Thank you for your cooperation.\nEnter to Continue\n");
+            Console.ReadKey();
+        }
+        public string ListTopics()
+        {
+            List<string> posts = Tools.ReadFileSplitComma(Social_Networking_System.PostFilePath);
+
+            List<string> topics = new List<string>();
+
+            string allTopics = "";
+
+            for (int i = 0; i < posts.Count; i = i + 4)
+            {
+                if (posts[i] == null || posts[i] == "")
+                {
+                    break;
+                }
+                string top = posts[i];
+                string title = posts[i + 1];
+                string tt = top + "," + title;
+                topics.Add(tt);
+            }
+
+            foreach (string t in topics)
+            {
+                allTopics = allTopics + t + "\n";
+            }
+
+            return allTopics;
+        }
+
+        public void AddTopic(Person p, string topic, string title, string body)
+        {
+
+            postWarning();
+
+            if (topic.Contains(",") || title.Contains(",") || body.Contains(","))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            Topic = topic;
+            Title = title;
+            Body = body;
+            Author = p.Username;
+
+            string line = Topic + "," + Title + "," + Body + "," + Author;
+
+            Tools.WriteFile(Social_Networking_System.PostFilePath, line);
+
+            Console.WriteLine("Topic added");
+        }
+
+        public void RemoveTopic(Person p, string topic, string title)
+        {
+            Topic = topic;
+            Title = title;
+            Author = p.Username;
+
+            List<string> posts = Tools.ReadFileSplitComma(Social_Networking_System.PostFilePath);
+
+            for (int i = 0; i < posts.Count; i = i + 4)
+            {
+                if (posts[i] == "")
+                {
+                    break;
+                }
+                string top = posts[i];
+                string tit = posts[i + 1];
+                string bod = posts[i + 2];
+                string aut = posts[i + 3];
+
+                if (Topic == top && Title == tit && Author == aut)
+                {
+                    posts.Remove(top);
+                    posts.Remove(tit);
+                    posts.Remove(bod);
+                    posts.Remove(aut);
+                }
+            }
+
+            string allPosts = "";
+
+            for (int i = 0; i < posts.Count; i = i + 4)
+            {
+
+                if (posts[i] == "")
+                {
+                    break;
+                }
+
+                string top = posts[i];
+                string tit = posts[i + 1];
+                string bod = posts[i + 2];
+                string aut = posts[i + 3];
+
+                allPosts = allPosts+ top + "," + tit + "," + bod + "," + aut + "\n";
+            }
+
+            Tools.WriteNotAppendingFile(Social_Networking_System.PostFilePath, allPosts);
+
+            Console.WriteLine("Topic Removed");
+
+        }
+    }
+
+    class Social_Networking_System
+    {
+
+        private static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public static string PersonFilePath = desktopPath + "\\Social_Networking_Users.txt";
+        public static string friendsFilePath = desktopPath + "\\Social_Network_Friends.txt";
+        public static string PostFilePath = desktopPath + "\\Social_Networking_Posts.txt";
+
+        public void ResetData()
+        {
+            Tools.DeleteFile(PersonFilePath);
+            Tools.DeleteFile(friendsFilePath);
+            Tools.DeleteFile(PostFilePath);
+
+        }
+
+        public void ExportData(string FilePath, int option)
+        {
+            if (option == 1)
+            {
+                File.Copy(PersonFilePath, FilePath);
+            }
+            else if (option == 2)
+            {
+                File.Copy(friendsFilePath, FilePath);
+            }
+            else if (option == 3)
+            {
+                File.Copy(PostFilePath, FilePath);
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not one of the options, the operation could not be completed.");
+            }
+
+        }
+
+        public void ImportData(string FilePath, int option)
+        {
+            if (option == 1)
+            {
+                File.Copy(FilePath, PersonFilePath);
+            }
+            else if (option == 2)
+            {
+                File.Copy(FilePath, friendsFilePath);
+            }
+            else if (option == 3)
+            {
+                File.Copy(FilePath, PostFilePath);
+            }
+            else
+            {
+                Console.WriteLine("Sorry that is not one of the options, the operation could not be completed.");
+            }
+        }
+    }
+
+    class Tools
+    {
+        private static void CheckForFile(string FilePath)
+        {
+            bool t = File.Exists(FilePath);
+
+            if (t == false)
+            {
+                CreateFile(FilePath);
+            }
+        }
+
+        private static void CreateFile(string FilePath)
+        {
+            try
+            {
+                File.Create(FilePath).Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Creating the file failed");
+                Console.WriteLine(e);
+                Console.Read();
+            }
+        }
+
+        public static void WriteFile(string FilePath, string line)
+        {
+            CheckForFile(FilePath);
+
+            using (StreamWriter sw = new StreamWriter(FilePath, append: true))
+            {
+                sw.WriteLine(line);
+                sw.Dispose();
+            }
+        }
+
+        public static List<string> ReadFileSplitComma(string FilePath)
+        {
+            CheckForFile(FilePath);
+
+            List<string> readInFile = new List<string>();
+
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                //SD: .Replace(Environment.NewLIne, ",") should remove the new line characters!
+                string all = sr.ReadToEnd().Replace(Environment.NewLine, ",");
+                readInFile = all.Split(',').ToList();
+                sr.Dispose();
+            }
+
+            return readInFile;
+        }
+
+        public static List<string> ReadFileSplitNewLine(string FilePath)
+        {
+            CheckForFile(FilePath);
+
+            List<string> readInFile = new List<string>();
+
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                //SD: .Replace(Environment.NewLIne, ",") should remove the new line characters!
+                string all = sr.ReadToEnd();
+                readInFile = all.Split('\n').ToList();
+                sr.Dispose();
+            }
+
+            return readInFile;
+        }
+
+        public static void WriteNotAppendingFile(string FilePath, string line)
+        {
+            CheckForFile(FilePath);
+
+            using (StreamWriter sw = new StreamWriter(FilePath, append: false))
+            {
+                sw.WriteLine(line);
+                sw.Dispose();
+            }
+        }
+
+        public static void DeleteFile(string FilePath)
+        {
+            File.Delete(FilePath);
+        }
+    }
+}
---
+++ Exercises/Results/dixo7800/Exam 3/Social_Network_Test.cs
@@ -0,0 +1,377 @@
+﻿using CS_350_Exam_3_Final_Version;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_350_Exam_3
+{
+    class Social_Network_Test
+    {
+        public static void AllTests()
+        {
+            try
+            {
+                RegisterUsersTest();
+                LoginTest();
+                AddFriendsTest();
+                ListFriendsTest();
+                RemoveFriendsTest();
+                AddTopicTest();
+                ListTopicsTest();
+                RemoveTopicTest();
+                ExportDataTest();
+                ImportDataTest();
+                ResetDataTest();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+
+        }
+
+        public static void RegisterUsersTest()
+        {
+            try
+            {
+                Console.WriteLine("Registration Test");
+
+                Person p = new Person();
+                p.Register("Bruce", "Wayne", "Batman@here.com", "Batman", "test");
+                Person t = new Person();
+                t.Register("Clark", "Kent", "Superman@here.com", "Superman", "pass");
+                Person x = new Person();
+                x.Register("Princess Diana", "Of Themyscira", "WW@here.com", "Wonder Woman", "demigod");
+                Person y = new Person();
+                y.Register("Logan", "X", "WeaponX@here.com", "Wolverine", "WeaponX");
+                Person z = new Person();
+                z.Register("Tony", "Stark", "IronMan@here.com", "Ironman", "pep");
+                Person a = new Person();
+                a.Register("Peter", "Parker", "FNSM@here.com", "Spiderman", "MJ");
+
+                Console.WriteLine("End of Registration.\nEnter to continue.\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void LoginTest()
+        {
+            try
+            {
+                Console.WriteLine("Login Test");
+
+                Person p = new Person();
+                p.Login("Batman", "test");
+                Person t = new Person();
+                t.Login("Superman", "pass");
+
+
+                Console.WriteLine("End of Loign.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void AddFriendsTest()
+        {
+            try
+            {
+                Console.WriteLine("Add Friends Test");
+
+                Person p = new Person();
+                p.Login("Batman", "test");
+
+                Person t = new Person();
+                t.Login("Superman", "pass");
+
+                Person x = new Person();
+                x.Login("Wonder Woman", "demigod");
+
+                Person y = new Person();
+                y.Login("Wolverine", "WeaponX");
+
+                Person z = new Person();
+                z.Login("Ironman", "pep");
+
+                Person a = new Person();
+                a.Login("Spiderman", "MJ");
+
+                Friend f = new Friend();
+                f.AddFriendship(p, t);
+                f.AddFriendship(p, x);
+                f.AddFriendship(z, a);
+                f.AddFriendship(y, a);
+                f.AddFriendship(p, z);
+
+                Console.WriteLine("End of adding friends.\nEnter to continue.\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void ListFriendsTest()
+        {
+            try
+            {
+                Console.WriteLine("List Friends Test");
+
+                Friend f = new Friend();
+
+                f.ListFriends();
+
+                Console.WriteLine("End of listing friends.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void RemoveFriendsTest()
+        {
+            try
+            {
+                Console.WriteLine("Remove friends test");
+
+                Person p = new Person();
+                Person t = new Person();
+                p.Login("Batman", "test");
+                t.Login("Superman", "pass");
+
+                Friend f = new Friend();
+                f.RemoveFriendship(p, t);
+
+                Console.WriteLine("End of removing friends.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void AddTopicTest()
+        {
+            try
+            {
+                Console.WriteLine("Add topic test");
+
+                Person p = new Person();
+                p.Login("Batman", "test");
+                Person t = new Person();
+                t.Login("Superman", "pass");
+                Person z = new Person();
+                z.Login("Ironman", "pep");
+
+
+                Post post = new Post();
+                post.AddTopic(p, "Science", "Science is cool!", "Science is amazing! There is a lot to learn from science and it has helped advance the world in many ways.");
+
+                Post post2 = new Post();
+                post2.AddTopic(t, "Programming", "Programming is fun", "Programming takes time to learn but once you do it is very powerful!");
+
+                Post post3 = new Post();
+                post3.AddTopic(z, "Industry", "The future of war", "The future of war will be won with mechanical soldiers");
+
+                Console.WriteLine("End of adding topics.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void ListTopicsTest()
+        {
+            try
+            {
+                Console.WriteLine("List topics test");
+
+                Post post = new Post();
+                string topics = post.ListTopics();
+                Console.WriteLine(topics);
+
+                Console.WriteLine("End of listing topics.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void RemoveTopicTest()
+        {
+            try
+            {
+                Console.WriteLine("Remove topic test");
+
+                Person t = new Person();
+                t.Login("Superman", "pass");
+                Post post = new Post();
+                post.RemoveTopic(t, "Programming", "Programming is fun");
+
+                Console.WriteLine("End of removing topics.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void ExportDataTest()
+        {
+            try
+            {
+                Console.WriteLine("Export data test");
+
+                string usersFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\users.txt";
+                string friendsFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\friends.txt";
+                string postsFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\posts.txt";
+
+                Social_Networking_System sys = new Social_Networking_System();
+
+                sys.ExportData(usersFile, 1);
+                sys.ExportData(friendsFile, 2);
+                sys.ExportData(postsFile, 3);
+
+
+                List<string> users = Tools.ReadFileSplitComma(usersFile);
+                Console.WriteLine("Users:\n");
+                foreach (string u in users)
+                {
+                    Console.WriteLine(u);
+                }
+                Console.WriteLine("\n");
+
+
+                List<string> friends = Tools.ReadFileSplitNewLine(friendsFile);
+                Console.WriteLine("Friends:\n");
+                foreach (string f in friends)
+                {
+                    Console.WriteLine(f);
+                }
+                Console.WriteLine("\n");
+
+
+                List<string> posts = Tools.ReadFileSplitComma(postsFile);
+                Console.WriteLine("Posts:\n");
+                foreach (string p in posts)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine("\n");
+
+                Console.WriteLine("End of exporting data.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+
+        }
+        
+        public static void ImportDataTest()
+        {
+            try
+            {
+                Console.WriteLine("Import Data test");
+
+                string usersFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\users.txt";
+                string friendsFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\friends.txt";
+                string postsFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\posts.txt";
+
+                Social_Networking_System sys = new Social_Networking_System();
+                sys.ResetData();
+                sys.ImportData(usersFile, 1);
+                sys.ImportData(friendsFile, 2);
+                sys.ImportData(postsFile, 3);
+
+                List<string> users = Tools.ReadFileSplitComma(Social_Networking_System.PersonFilePath);
+                Console.WriteLine("Users:\n");
+                foreach (string u in users)
+                {
+                    Console.WriteLine(u);
+                }
+                Console.WriteLine("\n");
+
+
+                List<string> friends = Tools.ReadFileSplitNewLine(Social_Networking_System.friendsFilePath);
+                Console.WriteLine("Friends:\n");
+                foreach (string f in friends)
+                {
+                    Console.WriteLine(f);
+                }
+                Console.WriteLine("\n");
+
+
+                List<string> posts = Tools.ReadFileSplitComma(Social_Networking_System.PostFilePath);
+                Console.WriteLine("Posts:\n");
+                foreach (string p in posts)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine("\n");
+
+                Console.WriteLine("End of importing data.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+
+        public static void ResetDataTest()
+        {
+            try
+            {
+                Console.WriteLine("Reset Data Test");
+
+                string usersFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\users.txt";
+                string friendsFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\friends.txt";
+                string postsFile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\posts.txt";
+                //Only needs to be done when tests are run.
+                Tools.DeleteFile(usersFile);
+                Tools.DeleteFile(friendsFile);
+                Tools.DeleteFile(postsFile);
+
+
+                Social_Networking_System sys = new Social_Networking_System();
+                sys.ResetData();
+
+                Console.WriteLine("End of resetting data.\nEnter to continue\n");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("An error has occured, operations have been stopped");
+            }
+            
+        }
+    }
+}
