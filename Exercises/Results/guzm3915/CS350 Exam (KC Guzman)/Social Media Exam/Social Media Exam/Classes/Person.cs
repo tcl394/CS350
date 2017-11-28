25f6f8916a97ced0c301f4b92254e4ca0578186d
UNC Software Engineering Class <32746485+UNC-CS350@users.noreply.github.com>
2017-11-28 14:08:07 -0700

    Add files via upload

---
+++ Exercises/Results/guzm3915/CS350 Exam (KC Guzman)/Social Media Exam/Social Media Exam/Classes/Person.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Media_Exam
+{
+    public class Person
+    {
+        //Public variables. User ID, post stuff, and friends. Posts and Friends are lists.
+        public string userIdentification
+        {
+            get;
+            set;
+        }
+        public string password
+        {
+            get;
+            set;
+        }
+        public static List<int>posts
+        {
+            get;
+            set;
+        }
+        public static List<string>friends
+        {
+            get;
+            set;
+        }
+
+        //friend control classes
+        public static bool addFriend(string newFriend)
+        {
+            try
+            {
+                if (friends.Contains(newFriend))
+                {
+                    return true;
+                }
+                else //if it doesnt contain the friend
+                {
+                    friends.Add(newFriend);
+                }
+                return true;    
+            }
+            catch
+            {
+                return false;
+            }
+           
+        }
+        public static bool removeFriend(string soonToBeNotAFriend)
+        {
+            try
+            {
+                if (friends.Contains(soonToBeNotAFriend))
+                {
+                    friends.Remove(soonToBeNotAFriend);
+                    //And now, as fortold by the variable profecy, he is no longer a friend
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        //Post control classes
+        public static bool addPost(int id)
+        {
+            try
+            {
+                if (posts.Contains(id))
+                {
+                    return true;
+                }
+                else //if it doesnt contain the post ID
+                {
+                    posts.Add(id);
+                }
+                return true;   
+            }
+            catch
+            {
+                return false;
+            }
+           
+        }
+        public static bool removePost(int id)
+        {
+            try
+            {
+                if (posts.Contains(id))
+                {
+                    posts.Remove(id);
+                }
+                //Begone, post!
+                return true;    
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ Exercises/Results/guzm3915/CS350 Exam (KC Guzman)/Social Media Exam/Social Media Exam/Classes/SystemFunc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Social_Media_Exam.Classes
+{
+    public class SystemFunc
+    {
+
+        public static List<Person> people = new List<Person>();
+        public static List<String> posts = new List<String>();
+        public static List<String> friends = new List<String>();
+
+        public static bool resetData()
+        {
+            try
+            {
+                File.Delete("data.csv");
+                people.Clear();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        public static bool exportData()
+        {
+            try
+            {
+                string output = "";
+                foreach (Person p in people)
+                {
+                    string posts = string.Join(":", p.posts);
+                    string friends = string.Join(":", p.friends);
+                    output += p.userIdentification + "," + p.password + "," + posts + "," + friends + Environment.NewLine;
+
+                }
+                File.WriteAllText("data.csv", output);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        //reads data in
+        public static bool importData()
+        {
+            try
+            {
+                foreach (string line in File.ReadAllLines("data.csv"))
+                {
+                    string[] linesplit = line.Split(',');
+                    string userID = linesplit[0];
+                    string password = linesplit[1];
+                    List<int> postID = new List<int>();
+                    List<string> friends = linesplit[3].Split(':').ToList<string>();
+                    foreach (string id in linesplit[2].Split(':'))
+                    {
+                        postID.Add(Convert.ToInt32(id));
+                    }
+
+                    people.Add(new Person()
+                    {
+                        userIdentification = userID,
+                        password = password,
+                        posts = postID,
+                        friends = friends
+                    });
+
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+    }
+}
